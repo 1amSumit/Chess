b4dd2bd7554a7697c8aec513a658b9e3ddcd068b
1amSumit <sj779619@gmail.com>
2023-05-14 12:21:28 +0530

    Home section fully implemented

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import classes from "./Hero.module.css";
+import chessHero from "../../assets/chesshero.jpg";
+import { GiChessPawn as Play } from "react-icons/gi";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <main className={classes.hero}>
+      <figure className={classes.img__part}>
+        <img className={classes.img} src={chessHero} alt="chess-board" />
+      </figure>
+      <div className={classes.content__part}>
+        <h1 className={classes.main__heading}>
+          <span>Play Chess</span>
+          <span>Online</span>
+          <span>on the #1 Site!</span>
+        </h1>
+        <div className={classes.btn}>
+          <button className={`${classes.btn__primary} ${classes.green_bg}`}>
+            <span>
+              {" "}
+              <Play /> Play Online
+            </span>
+            <p>play with someone at your level</p>
+          </button>
+          <button className={`${classes.btn__primary} ${classes.gray_bg}`}>
+            <span>
+              {" "}
+              <Play /> Play Online
+            </span>
+            <p>play with someone at your level</p>
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
