8e05844498b1fa8dd38de48543da1c22271933b4
1amSumit <sj779619@gmail.com>
2023-05-18 13:18:26 +0530

    another commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navigation from "../Navigation/Navigation";
 import Hero from "../Hero/Hero";
 import classes from "./Home.module.css";
+import Room from "../Room/Room";
 
 const Home = () => {
   return (
     <section className={classes.home__section}>
       <Navigation />
       <Hero />
+      <Room />
     </section>
   );
 };
